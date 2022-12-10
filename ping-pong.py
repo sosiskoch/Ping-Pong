815429ee5823f87da9756a2243947af576a483a5
sosiskoch <gglol18@bk.ru>
2022-12-10 14:32:53 +0300

    Рабочая среда

--- ping-pong.py
+++ ping-pong.py
@@ -5,9 +5,24 @@ W_H = 500
 
 window = display.set_mode((W_W, W_H))
 display.set_caption("Пинг-Понг")
-background = image.load("back.jpeg")
-background = transform.scale(background, (W_W, W_H))
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, fn, x, y, w, h, speed):
+        super().__init__()
+        self.image = image.load(fn)
+        self.image = transform.scale(self.image, (w, h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.w = w
+        self.h = h
+        self.speed = speed
+    def update(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+ball = GameSprite("ball_prev_ui.png", 100, 100, 50, 50, 0)
 clock = time.Clock()
 
 game = True
@@ -18,5 +33,6 @@ while game:
         if e.type == QUIT:
             game = False
 
-    window.blit(background, (0, 0))
+    window.fill((0,0,255))
+    ball.update()
     display.update()
